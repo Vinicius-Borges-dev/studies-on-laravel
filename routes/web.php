8bc131f1053519f09b5ea25991385858c9f519f7
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-09-14 18:44:12 -0300

    Criação: Criação de rota que só permite tipos pré definidos no parametro anterior à rota

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@ Route::get('/empresa', function(){
 Route::any('/any', function(){
     return 'Essa rota permite todo tipo de acesso http (put, delete, get, post)';
 });
+
+
+Route::match(['delete','post'],'/match', function(){
+    return 'Permite apenas acessos definidos';
+});
