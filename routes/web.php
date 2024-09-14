4909be3dcad26e138027a34ed0a17567c6e8a8c9
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-09-14 20:59:51 -0300

    Criação: Nomeando a rota /produto

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\ProdutoController;
 
 Route::get('/', [ProdutoController::class, 'index'] )->name('produto.index');
 
-Route::get('/produto/{id?}', [ProdutoController::class, 'show']);
+Route::get('/produto/{id?}', [ProdutoController::class, 'show'])->name('produto.show');
