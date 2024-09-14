6e29b755ee1bdab575a33e9fcf094ac5f5cc6a46
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-09-14 20:33:45 -0300

    Criação: Criação de grupo de rotas que utilizam o mesmo prefixo

--- routes/web.php
+++ routes/web.php
@@ -6,31 +6,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'dashboard';
+    });
 
-Route::view('/empresa', 'site/empresa');
+    Route::get('/users', function () {
+        return 'user';
+    });
 
-Route::any('/any', function(){
-    return 'Essa rota permite todo tipo de acesso http (put, delete, get, post)';
-});
-
-
-Route::match(['delete','post'],'/match', function(){
-    return 'Permite apenas acessos definidos';
-});
-
-
-Route::get('/produto/{id}/{categoria?}',function($id, $categoria = ''){
-    return "O id do produto é: {$id}<br>E a categoria é: {$categoria}";
-});
-
-
-Route::redirect('/sobre', 'empresa');
-
-
-Route::get('/news', function(){
-    return view('site/news');
-})->name('noticias');
-
-Route::get('/novidades', function(){
-    return redirect()->route('noticias');
+    Route::get('/clientes', function () {
+        return 'clientes';
+    });
 });
